7462b85e2f19e279c9d6631b8a9cc3ba2fcaef6d
Hem Raj Regmi <sangamsyabil@gmail.com>
2024-02-01 23:40:03 -0500

    more

--- GorebaGateway/video_display/models.py
+++ GorebaGateway/video_display/models.py
@@ -1,9 +1,15 @@
 from django.db import models
 
 
-class YouTubeVideo(models.Model):
-    title = models.CharField(max_length=255)
+class YouTubeVideos(models.Model):
+    # Video Information
+    video_title = models.CharField(max_length=255)
+    video_description = models.TextField()
     video_url = models.URLField()
 
+    # Timestamps
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
     def __str__(self):
-        return self.title
+        return self.video_title
--- GorebaGateway/visa_guidelines/models.py
+++ GorebaGateway/visa_guidelines/models.py
@@ -2,8 +2,15 @@ from django.db import models
 
 
 class VisaType(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.TextField()
+    # Visa Information
+    visa_name = models.CharField(max_length=100)
+    visa_description = models.TextField()
+    application_requirements = models.TextField()
+    processing_time = models.CharField(max_length=50)
+
+    # Timestamps
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.name
+        return self.visa_name
